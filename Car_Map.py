04766f8b54c51a03def424faf221d8e0559274a1
Joshua Basile <jbasile@g.ucla.edu>
2020-09-20 16:26:21 -0700

    Update Car_Map.py

--- Car_Map.py
+++ Car_Map.py
@@ -44,7 +44,7 @@ class Car:
             self.x = coords[0]
             self.y = coords[1]
             self.draw(canvas)
-        elif (reset_car == true):
+        elif (reset_car == True):
             self.x = 0
             self.y = 0
 
@@ -144,7 +144,7 @@ def check(car, obstacle_length, obstacle_start, SerialPort, canvas, window):
     sensor_string_decoded = str(sensor_string.decode('utf-8'))
     sensors = sensor_string_decoded.rstrip().split(',')
     if (sensors[0] != ''):
-        new_car = Car(SerialPort, false, canvas)
+        new_car = Car(SerialPort, False, canvas)
         car_queue.append(new_car)
 
         for i in range(0, len(sensors)):  # convert to doubles
@@ -208,7 +208,7 @@ def main():
     SerialPort = serial.Serial("COM5", "9600", timeout=1)
 
     # create car
-    car = Car(SerialPort, true, canvas)
+    car = Car(SerialPort, True, canvas)
 
     # car starts with nothing but wall in the right sensor
     obstacle_length = 0
